d412d08b1d071fcff711f6ead2d6cf30b2ea7bf0
Sanjeev S <sanjeevs@thoughtworks.com>
2019-11-01 23:14:16 +0530

    Implement hotel and print result

--- src/boardgame/Game.kt
+++ src/boardgame/Game.kt
@@ -1,6 +1,7 @@
 package boardgame
 
 class Game(val players: List<Player>, val board: List<String>, val dice: Dice, val configuration: Configuration){
+    private val hotelOwners:MutableMap<Int, Player> = mutableMapOf()
 
     fun play(){
         for(i in 1..Configuration.TOTAL_ROUNDS){
@@ -14,12 +15,23 @@ class Game(val players: List<Player>, val board: List<String>, val dice: Dice, v
             when (board[player.position]){
                 "J" -> player.amount = player.amount - Configuration.JAIL_FINE
                 "T" -> player.amount = player.amount + Configuration.TREASURE_BONUS
-                "H" -> {}
+                "H" -> {
+                    if(hotelOwners.containsKey(player.position)){
+                       val owner = hotelOwners[player.position]
+                        owner!!.amount = owner.amount + configuration.HOTEL_RENT
+                        player.amount = player.amount - configuration.HOTEL_RENT
+                    }
+                    else{
+                        player.amount = player.amount - configuration.HOTEL_AMOUNT
+                        hotelOwners[player.position] = player
+                    }
+                }
                 "E" -> {}
             }
         }
     }
 
     fun result() {
+        players.sortedBy { it.amount }.reversed().forEach { println("${it.name} => ${it.amount}")}
     }
 }
