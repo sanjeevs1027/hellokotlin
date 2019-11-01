058e8389e591e04df6dac601091dc688b6ef4da2
Sanjeev S <sanjeevs@thoughtworks.com>
2019-11-01 23:10:32 +0530

    Get players and game implementation

--- src/boardgame/App.kt
+++ src/boardgame/App.kt
@@ -2,17 +2,41 @@ package boardgame
 
 
 
+object Configuration {
+    const val JAIL_FINE = 150
+    const val TREASURE_BONUS = 200
+    const val HOTEL_RENT = 50
+    const val HOTEL_AMOUNT = 400
+    const val TOTAL_ROUNDS = 10
+    const val START_AMOUNT = 2000.00
+}
+
+data class Player(val name: String) {
+    var position: Int = 0
+    var amount: Double = Configuration.START_AMOUNT
+}
+
 fun main(){
     val board = getBoard()
     val dice = getDice()
     val players = getPlayers()
 
-
+    val game  =  Game(board=board, players = players, dice = dice, configuration = Configuration)
+    game.play()
+    game.result()
 }
 
-fun getPlayers(): Any?{
-    return null
+
+fun getPlayers(): List<Player> {
+    println("Enter number of players:")
+    val count = Integer.parseInt(readLine());
+    val players: MutableList<Player> = mutableListOf()
+    for (i in 1..count){
+        players.add(Player("Player$i"))
+    }
+    return players.toList()
 }
+
 
 fun getDice(): Dice {
     println("Enter the dice Input:")
---
+++ src/boardgame/Game.kt
@@ -0,0 +1,10 @@
+package boardgame
+
+class Game(val players: List<Player>, val board: List<String>, val dice: Dice, val configuration: Configuration){
+
+    fun play(){
+    }
+
+    fun result() {
+    }
+}
