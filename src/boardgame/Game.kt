3ce938270a40fa4630f9996275210750c656b065
Sanjeev S <sanjeevs@thoughtworks.com>
2019-11-01 23:12:52 +0530

    Implement jail treasure and empty

--- src/boardgame/Game.kt
+++ src/boardgame/Game.kt
@@ -3,6 +3,21 @@ package boardgame
 class Game(val players: List<Player>, val board: List<String>, val dice: Dice, val configuration: Configuration){
 
     fun play(){
+        for(i in 1..Configuration.TOTAL_ROUNDS){
+            round()
+        }
+    }
+
+    private fun round() {
+        for (player in players){
+            player.position = (player.position +  dice.roll()) % board.size
+            when (board[player.position]){
+                "J" -> player.amount = player.amount - Configuration.JAIL_FINE
+                "T" -> player.amount = player.amount + Configuration.TREASURE_BONUS
+                "H" -> {}
+                "E" -> {}
+            }
+        }
     }
 
     fun result() {
