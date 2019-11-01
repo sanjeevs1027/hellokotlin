04dc65f3242c21a8c5a90869ad57f979ac4472e1
Sanjeev S <sanjeevs@thoughtworks.com>
2019-11-01 22:39:37 +0530

    Game base

---
+++ src/boardgame/App.kt
@@ -0,0 +1,22 @@
+package boardgame
+
+
+
+fun main(){
+    val board = getBoard()
+    val dice = getDice()
+    val players = getPlayers()
+
+}
+
+fun getPlayers(): Any? {
+    return null
+}
+
+fun getDice(): Any? {
+    return null
+}
+
+fun getBoard():  Any? {
+    return null
+}
